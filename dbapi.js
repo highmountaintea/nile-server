77c9e54fb7baed28962368928155f6b711d53d59
jl818 <jl818@coolimage.com>
2018-03-01 00:15:39 -0500

    nile-server can now be run as npx

--- dbapi.js
+++ dbapi.js
@@ -1,11 +1,12 @@
+const path = require('path');
 const fs = require('fs-extra');
 
 async function loadDB() {
     let db = {};
-    db.users = JSON.parse(await fs.readFile('./db/users.json', 'utf8'));
-    db.products = JSON.parse(await fs.readFile('./db/products.json', 'utf8'));
-    db.reviews = JSON.parse(await fs.readFile('./db/reviews.json', 'utf8'));
-    db.hotitems = JSON.parse(await fs.readFile('./db/hotitems.json', 'utf8'));
+    db.users = JSON.parse(await fs.readFile(path.join(__dirname, 'db/users.json'), 'utf8'));
+    db.products = JSON.parse(await fs.readFile(path.join(__dirname, 'db/products.json'), 'utf8'));
+    db.reviews = JSON.parse(await fs.readFile(path.join(__dirname, 'db/reviews.json'), 'utf8'));
+    db.hotitems = JSON.parse(await fs.readFile(path.join(__dirname, 'db/hotitems.json'), 'utf8'));
     return db;
 }
 
